3b3af9e73e0f985361e1d40eb92fbaa734fb3d3d
Wai Pai Lee <pailee.wai@gmail.com>
2017-04-04 16:40:17 +0800

    Implemented react router

---
+++ src/client/component/page/hello.jsx
@@ -0,0 +1,13 @@
+// @flow-disable-next-line
+import React from 'react'
+
+import HelloButton from '../../container/hello-button'
+import Message from '../../container/message'
+
+const HelloPage = () =>
+  <div>
+    <Message />
+    <HelloButton />
+  </div>
+
+export default HelloPage
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { BrowserRouter } from 'react-router-dom'
 import thunkMiddleware from 'redux-thunk'
 
 import App from './app'
@@ -23,9 +24,11 @@ const rootEl = document.getElementById('main')
 
 const wrapApp = (AppComponent, reduxStore) =>
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>
 
 ReactDOM.render(wrapApp(App, store), rootEl)
